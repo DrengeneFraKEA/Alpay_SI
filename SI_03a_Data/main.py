356bf29bc2db1bd77b0b9637b915fa647880f1d0
ALPAYY1 <alpay@xmedicus.com>
2024-02-20 10:22:06 +0100

    Added 03a. Communicates with 02a.

---
+++ SI_03a_Data/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+import requests
+import json
+
+net_endpoint = "http://localhost:5298/SI02a/"
+
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return "Hello, World!"
+
+@app.route('/txt')
+def Txt():
+    url = net_endpoint + "txt"
+    return requests.get(url).text
+    
+@app.route('/xml')
+def Xml():
+    url = net_endpoint + "xml"
+    return requests.get(url).text
+
+@app.route('/csv')
+def Csv():
+    url = net_endpoint + "csv"
+    return requests.get(url).text
+
+@app.route('/yaml')
+def Yaml():
+    url = net_endpoint + "yaml"
+    return requests.get(url).text
+
+@app.route('/json')
+def Json():
+    url = net_endpoint + "json"
+    return requests.get(url).text
+
+if __name__ == '__main__':
+    app.run(port=8000)
