2a680bcaa0dd983d1c56a175e991cdf8ae5a09f3
ALPAYY1 <alpay@xmedicus.com>
2024-03-21 17:39:05 +0100

    04a_data added

---
+++ SI_04a_Data/SSE/Controllers/SSEController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace SSE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SSEController : ControllerBase
+    {
+        [HttpGet]
+        public async Task Home()
+        {
+            Response.Headers.Add("Content-Type", "text/event-stream");
+
+            while (true)
+            {
+                var currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                byte[] data = Encoding.UTF8.GetBytes(currentTime + "\n");
+
+                await Response.Body.WriteAsync(data, 0, data.Length);
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
